ca5984d7a7c902421ded7581a59856c33774c7b1
Jose Alvarez <jotajalvarez@gmail.com>
2026-04-19 01:27:28 -0500

    Agrega total al cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,10 +2,10 @@ import CartListItem from "@src/components/CartListItem";
 import { useCart } from "@src/providers/CartProviders";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { FlatList, Platform, View } from "react-native";
+import { Button, FlatList, Platform, Text, View } from "react-native";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
 
   return (
     <View>
@@ -14,6 +14,11 @@ const CartScreen = () => {
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={{ padding: 10, gap: 10 }}
       />
+
+      <Text style={{ marginTop: 20, fontWeight: "bold", fontSize: 20 }}>
+        Total: ${total}
+      </Text>
+      <Button title="Checkout" onPress={() => alert("Checkout")} />
 
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
--- src/providers/CartProviders.tsx
+++ src/providers/CartProviders.tsx
@@ -6,12 +6,14 @@ type CartContextType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
   updateQuantity: (id: string, quantity: -1 | 1) => void;
+  total: number;
 };
 
 const CartContext = createContext<CartContextType>({
   items: [],
   addItem: () => {},
   updateQuantity: () => {},
+  total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -48,8 +50,13 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     setItems(updatedItems);
   };
 
+  const total = items.reduce(
+    (sum, item) => sum + item.product.price * item.quantity,
+    0,
+  );
+
   return (
-    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );
